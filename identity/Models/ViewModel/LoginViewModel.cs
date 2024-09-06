7fe4cf506b3b4a0c30ad717142d242eea12175a5
iQraa <iQraa@DESKTOP-JJUA377>
2024-09-06 14:38:44 +0300

    Add project files.

---
+++ identity/Models/ViewModel/LoginViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations;
+using RequiredAttribute = Microsoft.Build.Framework.RequiredAttribute;
+
+namespace identity.Models.ViewModel
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [MaxLength(30)]
+        [Display(Name ="Email Address")]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name ="Password")]
+        public string Password { get; set; }
+        public bool RememberMe { get; set; } 
+
+    }
+}
---
+++ identity/Models/ViewModel/RegisterViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace identity.Models.ViewModel
+{
+    public class RegisterViewModel
+    {
+       
+        [Required]
+        [EmailAddress]
+        [MaxLength(30)]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [MaxLength(30)]
+        public string Password { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password))]
+        [MaxLength(30)]
+        public string ConfirmPassword { get; set; }
+        [Required]
+
+        public string Phone { get; set; }
+        
+    }
+}
